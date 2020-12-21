810cbb73af2ff70004dce35475b63b39a67694f4
Christo Lagali <christo.lagali@gmail.com>
2020-12-20 18:46:34 -0800

    Chnaged timeout period to 200

--- Threading Code/first_thread_code.py
+++ Threading Code/first_thread_code.py
@@ -6,7 +6,7 @@ def thread_function(name):
 
     logging.info("Thread %s: starting", name)
 
-    time.sleep(120)
+    time.sleep(200)
 
     logging.info("Thread %s: finishing", name) #Adding some comment here and more
 
